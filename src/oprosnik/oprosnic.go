1bbdbae9b9bf7e78718f9ddd6161829433b501ae
Anton Okolelov <varan@teasernet.com>
2015-04-27 23:10:53 +0300

    write questions to file

--- src/oprosnik/oprosnic.go
+++ src/oprosnik/oprosnic.go
@@ -5,6 +5,11 @@ import(
     "github.com/julienschmidt/httprouter"
     "net/http"
     "log"
+	//"strings"
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"regexp"
 )
 
 func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -13,10 +18,23 @@ func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 func admin(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	render(w, "admin-form.html")
+	
 }
 
 func save(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-
+    text := r.FormValue("words")
+	
+	words := regexp.MustCompile("\r\n").Split(text, 1000)
+	wordsJson, _ := json.Marshal(words)
+	f, err := os.Create("data/questions.json")
+	if err != nil {
+		path, _ := filepath.Abs(".")
+		log.Fatal("File  " + path, err)
+	}
+	f.Write(wordsJson)
+	f.Close()
+	http.Redirect(w, r, "/admin", 302)
+	
 }
 
 func Start() {
